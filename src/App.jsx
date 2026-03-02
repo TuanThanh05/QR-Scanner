2b64e4c1b475bafef931368f50f65f4372398195
letramanh <letramanh2182005@gmail.com>
2026-03-02 09:09:05 +0700

    Demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import { Routes ,Route} from 'react-router-dom'
+import CodePage from './pages/Code'
+import Home from './pages/Home'
 import AboutPage from './pages/About'
 function App() {
  
 
   return (
    <Routes>
-    <Route path="/" element={<p>Home Page</p>}/>
-    <Route path='/about' element={<AboutPage/>}/>
+    <Route path="/" element={<Home/>}/>
+    <Route path='/code' element={<CodePage/>}/>
+     <Route path='/about' element={<AboutPage/>}/>
    </Routes>
   )
 }
